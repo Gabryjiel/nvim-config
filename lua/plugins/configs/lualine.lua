7a102497027c0b434f003c7ef9a13f3b939d9794
Gabryjiel <gabriel.kudyba@gmail.com>
2024-07-21 17:05:51 +0200

    Set lualine theme to catpuccin

---
+++ lua/plugins/configs/lualine.lua
@@ -0,0 +1,27 @@
+local lualine = require("lualine")
+
+lualine.setup({
+	options = {
+		theme = "catpuccin",
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "filename" },
+		lualine_c = {},
+		lualine_x = {
+			{ "encoding" },
+			{ "fileformat" },
+			{ "filetype" },
+		},
+		lualine_y = {},
+		lualine_z = { "location" },
+	},
+	tabline = {
+		lualine_a = { "tabs" },
+		lualine_b = { "branch" },
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { "windows" },
+	},
+})
