694ed2481c2ce8480f1f3f345dd8a41bfa50247c
Gabryjiel <gabriel.kudyba@gmail.com>
2025-01-23 11:31:43 +0100

    cleanup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,69 +4,12 @@ return {
 		name = "catppuccin",
 		lazy = false,
 		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+			})
+
+			vim.cmd.colorscheme("catppuccin")
+		end,
 	},
-	{
-		"comfysage/evergarden",
-		priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-		opts = {
-			transparent_background = true,
-			variant = "medium", -- 'hard'|'medium'|'soft'
-			overrides = {}, -- add custom overrides
-		},
-	},
-	{
-		"ficcdaf/ashen.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"olivercederborg/poimandres.nvim",
-		lazy = false,
-		priority = 1000,
-		-- config = function()
-		-- 	require("poimandres").setup({
-		-- 		-- leave this setup function empty for default config
-		-- 		-- or refer to the configuration section
-		-- 		-- for configuration options
-		-- 	})
-		-- end,
-		--
-		-- -- optionally set the colorscheme within lazy config
-		-- init = function()
-		-- 	vim.cmd("colorscheme poimandres")
-		-- end,
-	},
-	{
-		"ilof2/posterpole.nvim",
-		priority = 1000,
-		lazy = false,
-		opts = {},
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{ "EdenEast/nightfox.nvim", priority = 1000, opts = {} },
-	{ "Mofiqul/vscode.nvim", priority = 1000, opts = {} },
-	{ "shaunsingh/nord.nvim", priority = 1000 },
-	{ "AlexvZyl/nordic.nvim", priority = 1000, opts = {} },
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		priority = 1000,
-	},
-	{ "tiagovla/tokyodark.nvim", priority = 1000, opts = {} },
-	{ "sainnhe/everforest", priority = 1000 },
-	{ "marko-cerovac/material.nvim", priority = 1000 },
-	{ "scottmckendry/cyberdream.nvim", lazy = false, priority = 1000 },
-	{ "navarasu/onedark.nvim", priority = 1000 },
-	{ "tiagovla/tokyodark.nvim", priority = 1000, opts = {} },
 }
--- lua/plugins/move.lua
+++ lua/plugins/move.lua
@@ -3,4 +3,12 @@ return {
 	opts = {
 		--- Config
 	},
+	keys = {
+		{ "<A-j>", "<cmd>MoveLine(1)<cr>", desc = "Move line up", silent = true, noremap = true },
+		{ "<A-k>", "<cmd>MoveLine(-1)<cr>", desc = "Move line down", silent = true, noremap = true },
+		{ "<A-h>", "<cmd>MoveHChar(-1)<cr>", desc = "Move char left", silent = true, noremap = true },
+		{ "<A-l>", "<cmd>MoveHChar(1)<cr>", desc = "Move char right", silent = true, noremap = true },
+		{ "<A-w>", "<cmd>MoveWord(1)<cr>", desc = "Move word forward", silent = true, noremap = true },
+		{ "<A-b>", "<cmd>MoveWord(-1)<cr>", desc = "Move word backwards", silent = true, noremap = true },
+	},
 }
