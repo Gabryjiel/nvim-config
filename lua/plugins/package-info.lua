b1b582b57bfba1b8c075a3a837a37b7933a02f7e
Gabryjiel <gabriel.kudyba@gmail.com>
2025-02-09 19:19:07 +0100

    fix: resolve conflicting keybindings
    Conflicting packages:
    - Snacks
    - nvim-lint
    - package-info

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -1,69 +1,28 @@
 return {
-	"vuki656/package-info.nvim",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		local package_info = require("package-info")
-		package_info.setup({
-			colors = {
-				up_to_date = "237",
-				outdated = "173",
-			},
-			style = {
-				up_to_date = "| + ", -- Icon for up to date dependencies
-				outdated = "| - ", -- Icon for outdated dependencies
-			},
-			hide_up_to_date = true,
-		})
-
-		vim.keymap.set(
-			{ "n" },
-			"<LEADER>ns",
-			package_info.show,
-			{ silent = true, noremap = true, desc = "Show dependency version" }
-		)
-
-		vim.keymap.set(
-			{ "n" },
-			"<LEADER>nc",
-			package_info.hide,
-			{ silent = true, noremap = true, desc = "Hide dependency version" }
-		)
-
-		vim.keymap.set(
-			{ "n" },
-			"<LEADER>nt",
-			package_info.toggle,
-			{ silent = true, noremap = true, desc = "Toggle dependency versions" }
-		)
-
-		vim.keymap.set(
-			{ "n" },
-			"<LEADER>nu",
-			package_info.update,
-			{ silent = true, noremap = true, desc = "Update dependency on the line" }
-		)
-
-		vim.keymap.set(
-			{ "n" },
-			"<LEADER>nd",
-			package_info.delete,
-			{ silent = true, noremap = true, desc = "Delete dependency on the line" }
-		)
-
-		vim.keymap.set(
-			{ "n" },
-			"<LEADER>ni",
-			package_info.install,
-			{ silent = true, noremap = true, desc = "Install a new dependency" }
-		)
-
-		vim.keymap.set(
-			{ "n" },
-			"<LEADER>np",
-			package_info.change_version,
-			{ silent = true, noremap = true, desc = "Install a different dependency version" }
-		)
-	end,
+  "vuki656/package-info.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    colors = {
+      up_to_date = "237",
+      outdated = "173",
+    },
+    style = {
+      up_to_date = "| + ", -- Icon for up to date dependencies
+      outdated = "| - ", -- Icon for outdated dependencies
+    },
+    hide_up_to_date = true,
+  },
+  keys = {
+    { "<leader>nt", "<cmd>lua require('package-info').toggle()<cr>", desc = "Toggle dependency version" },
+    { "<leader>nu", "<cmd>lua require('package-info').update()<cr>", desc = "Update dependency on the line" },
+    { "<leader>nd", "<cmd>lua require('package-info').delete()<cr>", desc = "Delete dependency on the line" },
+    { "<leader>ni", "<cmd>lua require('package-info').install()<cr>", desc = "Install a new dependency" },
+    {
+      "<leader>nc",
+      "<cmd>lua require('package-info').change_version()()<cr>",
+      desc = "Install a different dependency version",
+    },
+  },
 }
